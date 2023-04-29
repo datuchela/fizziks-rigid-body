861507d5ed9d442be46d074b4767a3b57326e444
datuchela <davit.narimanidze408@gmail.com>
2023-04-29 17:59:42 +0400

    updated imports

--- src/engine/init.ts
+++ src/engine/init.ts
@@ -1,51 +1,51 @@
-import { Square } from "../objects/Square";
+import { Square } from "./objects/Square";
 import { relativePoint } from "../utils/relativePoint";
 import { engineState } from "./engineState";
 
 export type Engine = {
-  onClickCanvas: (clientX: number, clientY: number) => void;
+	onClickCanvas: (clientX: number, clientY: number) => void;
 };
 
 const DEFAULT_CANVAS_WIDTH = 1280;
 const DEFAULT_CANVAS_HEIGHT = 720;
 
 const handleClick =
-  (canvas: HTMLCanvasElement) => (clientX: number, clientY: number) => {
-    const [x, y] = relativePoint(clientX, clientY, canvas);
-    const square = new Square({ x, y, mass: 50 });
-    engineState.addObject(square);
-  };
+	(canvas: HTMLCanvasElement) => (clientX: number, clientY: number) => {
+		const [x, y] = relativePoint(clientX, clientY, canvas);
+		const square = new Square({ x, y, mass: 50 });
+		engineState.addObject(square);
+	};
 
 export const init = (canvasNode: HTMLCanvasElement) => {
-  let elapsed = 0;
-  let oldTimeStamp = 0;
+	let elapsed = 0;
+	let oldTimeStamp = 0;
 
-  const canvas = canvasNode;
-  const ctx = canvas?.getContext("2d");
-  if (!ctx) return;
+	const canvas = canvasNode;
+	const ctx = canvas?.getContext("2d");
+	if (!ctx) return;
 
-  canvas.width = DEFAULT_CANVAS_WIDTH;
-  canvas.height = DEFAULT_CANVAS_HEIGHT;
+	canvas.width = DEFAULT_CANVAS_WIDTH;
+	canvas.height = DEFAULT_CANVAS_HEIGHT;
 
-  const tick = (timeStamp: number) => {
-    elapsed = (timeStamp - oldTimeStamp) / 1000;
-    oldTimeStamp = timeStamp;
-    elapsed = Math.min(elapsed, 0.1);
+	const tick = (timeStamp: number) => {
+		elapsed = (timeStamp - oldTimeStamp) / 1000;
+		oldTimeStamp = timeStamp;
+		elapsed = Math.min(elapsed, 0.1);
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    engineState.detectAndHandleCollisions();
-    engineState.detectAndHandleEdgeCollisions(canvas);
+		engineState.detectAndHandleCollisions();
+		engineState.detectAndHandleEdgeCollisions(canvas);
 
-    engineState.updateObjects(elapsed);
-    engineState.drawObjects(ctx);
+		engineState.updateObjects(elapsed);
+		engineState.drawObjects(ctx);
 
-    requestAnimationFrame(tick);
-  };
+		requestAnimationFrame(tick);
+	};
 
-  requestAnimationFrame(tick);
+	requestAnimationFrame(tick);
 
-  return {
-    onClickCanvas: handleClick(canvas),
-  };
+	return {
+		onClickCanvas: handleClick(canvas),
+	};
 };
