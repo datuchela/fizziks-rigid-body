f2ddd151648259b7696672b90ddfa9518571fa34
datuchela <davit.narimanidze408@gmail.com>
2023-06-16 19:52:02 +0400

    refactor: add mass and material as params in spawnObject function

--- src/engine/init.ts
+++ src/engine/init.ts
@@ -1,53 +1,81 @@
 import { EngineObject, EngineState } from "./engineState";
+import { Material } from "./objects";
 import { Circle } from "./objects/Circle";
 
 export type Engine = {
-	spawnObject: (x: number, y: number) => void;
+  spawnObject: ({
+    x,
+    y,
+    mass,
+    material,
+  }: {
+    x: number;
+    y: number;
+    mass: number;
+    material: Material;
+  }) => void;
 };
 
-const addObject = (engineState: EngineState) => (x: number, y: number) => {
-	const circle = new Circle({ x, y, mass: 50 });
-	engineState.addObject(circle);
-};
+const addObject =
+  (engineState: EngineState) =>
+  ({
+    x,
+    y,
+    mass,
+    material,
+  }: {
+    x: number;
+    y: number;
+    mass: number;
+    material: Material;
+  }) => {
+    const circle = new Circle({
+      x,
+      y,
+      mass,
+      material,
+    });
+    engineState.addObject(circle);
+  };
 
 export type EngineInitParams = {
-	canvasWidth: number;
-	canvasHeight: number;
-	onBeforeUpdate?: () => void;
-	onUpdate?: (objects: EngineObject[]) => void;
+  canvasWidth: number;
+  canvasHeight: number;
+  onBeforeUpdate?: () => void;
+  onUpdate?: (objects: EngineObject[]) => void;
 };
 
 export const init = ({
-	canvasWidth,
-	canvasHeight,
-	onBeforeUpdate,
-	onUpdate,
+  canvasWidth,
+  canvasHeight,
+  onBeforeUpdate,
+  onUpdate,
 }: EngineInitParams) => {
-	const engineState = new EngineState(canvasWidth, canvasHeight);
+  const engineState = new EngineState(canvasWidth, canvasHeight);
 
-	let elapsed = 0;
-	let oldTimeStamp = 0;
+  let elapsed = 0;
+  let oldTimeStamp = 0;
 
-	const tick = (timeStamp: number) => {
-		elapsed = (timeStamp - oldTimeStamp) / 1000;
-		oldTimeStamp = timeStamp;
-		elapsed = Math.min(elapsed, 0.1);
+  const tick = (timeStamp: number) => {
+    elapsed = (timeStamp - oldTimeStamp) / 1000;
+    oldTimeStamp = timeStamp;
+    elapsed = Math.min(elapsed, 0.1);
 
-		onBeforeUpdate && onBeforeUpdate();
+    onBeforeUpdate && onBeforeUpdate();
 
-		engineState.detectCollisions();
-		engineState.detectEdgeCollisions();
+    engineState.detectCollisions();
+    engineState.detectEdgeCollisions();
 
-		engineState.updateObjects(elapsed);
+    engineState.updateObjects(elapsed);
 
-		onUpdate && onUpdate(engineState.objects);
+    onUpdate && onUpdate(engineState.objects);
 
-		requestAnimationFrame(tick);
-	};
+    requestAnimationFrame(tick);
+  };
 
-	requestAnimationFrame(tick);
+  requestAnimationFrame(tick);
 
-	return {
-		spawnObject: addObject(engineState),
-	};
+  return {
+    spawnObject: addObject(engineState),
+  };
 };
