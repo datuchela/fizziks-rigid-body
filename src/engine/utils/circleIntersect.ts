0145f3060384cd17a7de51efe655786df32e4505
datuchela <davit.narimanidze408@gmail.com>
2023-06-16 14:42:18 +0400

    fix: circle intersection implementation

--- src/engine/utils/circleIntersect.ts
+++ src/engine/utils/circleIntersect.ts
@@ -1,13 +1,12 @@
 import { Circle } from "../objects/Circle";
+import { square } from "./square";
 
 export const circleIntersect = (circle1: Circle, circle2: Circle) => {
-	if (
-		circle2.x > circle1.radius * 2 + circle1.x ||
-		circle1.x > circle2.radius * 2 + circle2.x ||
-		circle2.y > circle1.radius * 2 + circle1.y ||
-		circle1.y > circle2.radius * 2 + circle2.y
-	) {
-		return false;
-	}
-	return true;
+  // Calculate the distance between the two circles
+  const squareDistance =
+    square(circle1.x - circle2.x) + square(circle1.y - circle2.y);
+
+  // When the distance is smaller or equal to the sum
+  // of the two radius, the circles touch or overlap
+  return squareDistance <= square(circle1.radius + circle2.radius);
 };
