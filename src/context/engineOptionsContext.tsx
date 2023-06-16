bf140d635b1c07f199b8bb235291dc285b451802
datuchela <davit.narimanidze408@gmail.com>
2023-06-16 19:48:56 +0400

    feat: create engineOptionsContext

---
+++ src/context/engineOptionsContext.tsx
@@ -0,0 +1,31 @@
+import { createContext, useState, useMemo } from "react";
+import { Material } from "../engine/objects";
+
+interface EngineContextProps {
+  mass: number;
+  material: Material;
+  setMass: React.Dispatch<React.SetStateAction<number>>;
+  setMaterial: React.Dispatch<React.SetStateAction<Material>>;
+}
+
+export const engineOptionsContext = createContext({
+  mass: 0,
+  material: Material.Silicon,
+} as EngineContextProps);
+
+export const EngineOptionsContextProvider = ({
+  children,
+}: React.PropsWithChildren) => {
+  const [mass, setMass] = useState(0);
+  const [material, setMaterial] = useState(Material.Silicon);
+
+  const memoizedState = useMemo(() => {
+    return { mass, material, setMass, setMaterial };
+  }, [mass, material, setMass, setMaterial]);
+
+  return (
+    <engineOptionsContext.Provider value={memoizedState}>
+      {children}
+    </engineOptionsContext.Provider>
+  );
+};
