85554220b6f5b6aafcd85c76c01f860e62c3cd7e
datuchela <davit.narimanidze408@gmail.com>
2023-06-20 22:52:21 +0400

    fix: convert density unit to 2d

--- src/engine/objects/materials.constants.ts
+++ src/engine/objects/materials.constants.ts
@@ -1,9 +1,15 @@
 export enum Material {
-  Silicon = "silicon",
-  Aluminium = "aluminium",
+  Silicon = "Silicon",
+  Aluminium = "Aluminium",
+  Platinum = "Platinum",
 }
 
+// Since given density units are in kg/m^3,
+// and our engine only cares about 2D space we need to convert densities into kg/m^2.
+const convertTo2d = (density: number) => Math.pow(Math.cbrt(density), 2);
+
 export const densities = new Map([
-  [Material.Silicon, 20],
-  [Material.Aluminium, 70],
+  [Material.Silicon, convertTo2d(2330)],
+  [Material.Aluminium, convertTo2d(2700)],
+  [Material.Platinum, convertTo2d(21450)],
 ]);
