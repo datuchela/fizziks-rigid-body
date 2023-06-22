fbdd2f8fdc27daae60cfd9201d769eb94549d527
datuchela <davit.narimanidze408@gmail.com>
2023-06-22 16:48:45 +0400

    refactor: add setCoordinates, setVelocity methods

--- src/engine/objects/BaseObject.ts
+++ src/engine/objects/BaseObject.ts
@@ -1,3 +1,4 @@
+import type { Vector } from "../types";
 import { GRAVITY_EARTH } from "../utils/constants";
 
 export interface BaseObjectConstructorProps {
@@ -16,12 +17,21 @@ export interface BaseObject {
 	vx: number;
 	vy: number;
 	mass: number;
+	setCoordinates: (setterFn: ([x, y]: Vector) => Vector) => void;
+	setVelocity: (setterFn: ([vx, vy]: Vector) => Vector) => void;
 	update: (dt: number) => void;
 	draw: (ctx: CanvasRenderingContext2D) => void;
 }
 
 export class BaseObject {
-	constructor({ id, x, y, vx = 0, vy = 0, mass = 10 }: BaseObjectConstructorProps) {
+	constructor({
+		id,
+		x,
+		y,
+		vx = 0,
+		vy = 0,
+		mass = 10,
+	}: BaseObjectConstructorProps) {
 		this.id = id ?? crypto.randomUUID();
 		this.x = x;
 		this.y = y;
@@ -30,13 +40,24 @@ export class BaseObject {
 		this.mass = mass;
 	}
 
+	setCoordinates = (setterFn: ([x, y]: Vector) => Vector) => {
+		const [newX, newY] = setterFn([this.x, this.y]);
+		this.x = newX;
+		this.y = newY;
+	};
+
+	setVelocity = (setterFn: ([vx, vy]: Vector) => Vector) => {
+		const [newVx, newVy] = setterFn([this.vx, this.vy]);
+		this.vx = newVx;
+		this.vy = newVy;
+	};
+
 	update = (dt: number) => {
 		this.fall(dt);
-		this.x += this.vx * dt;
-		this.y += this.vy * dt;
+		this.setCoordinates(([x, y]) => [x + this.vx * dt, y + this.vy * dt]);
 	};
 
 	private fall = (dt: number) => {
-		this.vy += dt * GRAVITY_EARTH;
+		this.setVelocity(([vx, vy]) => [vx, vy + dt * GRAVITY_EARTH]);
 	};
 }
